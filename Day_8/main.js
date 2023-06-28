ac84043b01f743e416f68cf4d181d6ccd128c792
maulik kansoda <maulikkwork@gmail.com>
2023-06-28 13:57:14 +0530

    day 1 to 9 done

---
+++ Day_8/main.js
@@ -0,0 +1,29 @@
+// Function to perform the calculation
+function calculate() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var operator = document.getElementById('operator').value;
+    var result;
+  
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num1 / num2;
+        break;
+      default:
+        result = "Invalid operator";
+    }
+  
+    document.getElementById('result').innerHTML = "Result: " + result;
+  }
+  
+  // Event listener for the calculate button
+  document.getElementById('calculateBtn').addEventListener('click', calculate);
